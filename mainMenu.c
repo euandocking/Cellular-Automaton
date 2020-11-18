aff21f37a56d1b147289f19ed2a136a9b738b546
euandocking <55397929+euandocking@users.noreply.github.com>
2020-11-18 15:45:15 +0000

    length and width user input, loop menu
    length and width are input by the user
    the menu was changed to use a while loop instead of acting recursively

--- mainMenu.c
+++ mainMenu.c
@@ -3,29 +3,12 @@
 #include <stdio.h>
 #include "automata.h"
 
-/*
-Declaring functions
-*/
-void displayMenu();
-
 void processChoice();
-
 
 int main()
 {
-	displayMenu();
-	return 0;
-}
-
-/*
-Displays the menu to the user
-*/
-void displayMenu()
-{
-	//display the options
-	printf("Main Menu:\n1. Generate Cellular Automaton\n2. option 2\n3. option 3\n4. Exit\n");
-	//call the function to process user input
 	processChoice();
+	return 0;
 }
 
 /*
@@ -33,33 +16,41 @@ Asks the user for an input and processes their choice
 */
 void processChoice()
 {
-	int choice;//vairable storing the user's choice
+	int choice = -1;//vairable storing the user's choice
 
-	printf("Please enter your choice\n");
-	scanf("%d", &choice);
-		
-	switch(choice)
+	int width = 30;
+	int length = 10;
+
+	while (choice != 0)
 	{
-		case 1://Generate cellular automata
-			generateAutomata();
-			displayMenu();
-			break;
-		case 2://placeholder option
-			printf("Not implemented\n");
-			displayMenu();
-			break;
-		case 3://placeholder option
-			printf("Not implemented\n");
-			displayMenu();
-			break;
-		case 4://Exit the program
-			printf("Goodbye.\n");
-			break;
-		default://default case, handling invalid user input
-			printf("Invalid selection\n");
-			displayMenu();
-			break;
+		//display current settings
+		printf("Current settings:\nWidth:%d\nLength:%d\n", width, length);
+
+		//display the options
+		printf("Main Menu:\n1. Generate Cellular Automaton\n2. Set Width\n3. Set Length\n0. Exit\n");
+		printf("Please enter your choice\n");
+		scanf("%d", &choice);
+
+		switch(choice)
+		{
+			case 1://Generate cellular automata
+				generateAutomata(width, length);
+				break;
+			case 2://Set width
+				printf("Enter the width for the cell generations\n");
+				scanf("%d", &width);
+				break;
+			case 3://Set length
+				printf("Enter the number of generations to produce\n");
+				scanf("%d", &length);
+				break;
+			case 0://Exit the program
+				printf("Goodbye.\n");
+				break;
+			default://default case, handling invalid user input
+				printf("Invalid selection\n");
+				break;
 		}
-	
+	}
 }
 
